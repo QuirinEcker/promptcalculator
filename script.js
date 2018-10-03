9bd06ea0dd4d9a713bee8f0a0d98f83bb4d453ca
QuirinEcker <quirin.ecker@bajupa.com>
2018-10-03 19:40:23 +0200

    initial finsihed code

---
+++ script.js
@@ -0,0 +1,10 @@
+let number1;
+let number2;
+
+number1 = prompt("Geben sie die Zahl1 ein: ");
+number2 = prompt("Geben sie die Zahl2 ein: ");
+
+number1 = parseInt(number1);
+number2 = parseInt(number2);
+
+alert(number1 + number2);
